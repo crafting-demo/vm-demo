105e502d07f82ae518211c0515a9463c6f5ca97f
Yisui Hu <hu@crafting.dev>
2023-08-08 23:34:35 +0000

    update scripts

--- dotnet/eShopOnWeb/scripts/run.ps1
+++ dotnet/eShopOnWeb/scripts/run.ps1
@@ -1,4 +1,8 @@
 Set-Location -Path "C:\Users\Administrator\Desktop\eShopOnWeb\src\Web"
 $env:UseOnlyInMemoryDatabase = "true"
 $env:RedisServerUrl = "localhost:6379"
-dotnet run --launch-profile Web
+$prev = Import-Clixml -Path (Join-Path $ENV:temp 'sandbox-process.xml')
+$prev | Stop-Process -ErrorAction SilentlyContinue
+$process = Start-Process "dotnet" -ArgumentList "run --launch-profile Web" -PassThru
+$process | Export-Clixml -Path (Join-Path $ENV:temp 'sandbox-process.xml')
+Wait-Process -Id $process.Id
