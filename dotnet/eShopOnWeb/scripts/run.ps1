61f46149a0a2627cdcda73e412d3647c0e6d125e
Yisui Hu <hu@crafting.dev>
2023-08-09 00:57:37 +0000

    update sandbox-process file path

--- dotnet/eShopOnWeb/scripts/run.ps1
+++ dotnet/eShopOnWeb/scripts/run.ps1
@@ -1,8 +1,8 @@
 Set-Location -Path "C:\Users\Administrator\Desktop\eShopOnWeb\src\Web"
 $env:UseOnlyInMemoryDatabase = "true"
 $env:RedisServerUrl = "localhost:6379"
-$prev = Import-Clixml -Path (Join-Path $ENV:temp 'sandbox-process.xml')
+$prev = Import-Clixml -Path (Join-Path 'C:\Users\Administrator\Desktop' 'sandbox-process.xml')
 $prev | Stop-Process -ErrorAction SilentlyContinue
 $process = Start-Process "dotnet" -ArgumentList "run --launch-profile Web" -PassThru
-$process | Export-Clixml -Path (Join-Path $ENV:temp 'sandbox-process.xml')
+$process | Export-Clixml -Path (Join-Path 'C:\Users\Administrator\Desktop' 'sandbox-process.xml')
 Wait-Process -Id $process.Id
